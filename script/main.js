7adaa00a00ac6c98b74af056fa4bfacd6c79fc07
alessandro <alessandro.massarella@gmail.com>
2020-12-07 15:07:33 +0100

    MS-4 (fix)

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
-const API_KEY = '02a542c8f8c1d3909cadbfdcc1deab24'
+const API_KEY = '02a542c8f8c1d3909cadbfdcc1deab24';
+const movieUrl = 'https://api.themoviedb.org/3/search/movie';
+const serieUrl = 'https://api.themoviedb.org/3/search/tv'
 
 const myApp = new Vue ({
 
@@ -16,35 +18,28 @@ const myApp = new Vue ({
 
   methods: {
     submitSearch: function() {
-      this.isVisible= true, 
-      axios
-      // RICERCA FILM
-            .get ('https://api.themoviedb.org/3/search/movie', {
-              params: {
-                'api_key': API_KEY,
-                'language': 'it',
-                'query': this.querySearch
-              }
-            })
-            .then ( (result) => {
-              console.log(result)
-              this.movieSearch = result.data.results
-              // console.log(this.movieSearch)
-            })
+      this.isVisible= true,
 
-      // RICERCA SERIE TV
-      axios
-            .get ('https://api.themoviedb.org/3/search/tv', {
+    axios.all([
+      axios.get(movieUrl, {
+              params: {
+                'api_key': API_KEY,
+                'language': 'it',
+                'query': this.querySearch
+              }
+            }),
+      axios.get(serieUrl, {
               params: {
                 'api_key': API_KEY,
                 'language': 'it',
                 'query': this.querySearch
               }
             })
-            .then ( (result) => {
-              console.log(result)
-              this.serieSearch = result.data.results
-            })
+    ])
+          .then(axios.spread((result1, result2) => {
+            this.movieSearch = result1.data.results;
+            this.serieSearch = result2.data.results;
+          }));
 
 
     },
